a00192530657fe2b0c7b93c237bc58f539126b48
Salma116 <s.elmourabet@gmail.com>
2021-11-05 16:04:42 +0100

    add Title field to Bdapost

--- models/BdaPost.go
+++ models/BdaPost.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 type BdaPost struct {
 	gorm.Model
+	Title   string `json:"title" binding:"required,min=4,max=100"`
 	Content string `json:"content" binding:"required,min=4,max=1024"`
 	UserID  uint   `json:"user_id"`
 }
