702c1267ef099760e254ff70233b3cc453cb2c42
fanny.armand <fanny.armand@adevinta.com>
2021-12-31 11:16:35 +0100

    chore: update script for easy release

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/env bash
 
-IMAGE_LATEST=ghcr.io/ada-social-network/api:latest
-IMAGE_VERSION=ghcr.io/ada-social-network/api:"${1}"
+# check argument count ($#) is equal 0
+if [ $# -eq 0 ]
+  then
+    printf "No arguments supplied.\n\n"
+    printf "Please provide one argument with the expected version.\n"
+    printf "For example: \"./scripts/release.sh v1.0.0\"\n"
+    exit 1
+fi
+
+VERSION="${1}"
+LATEST="latest"
+IMAGE_NAME="ghcr.io/ada-social-network/api"
+
+IMAGE_LATEST="${IMAGE_NAME}:${LATEST}"
+IMAGE_VERSION="${IMAGE_NAME}:${VERSION}"
 
 printf "Build image with TAG:\n"
 printf "  - latest: %s\n" "${IMAGE_LATEST}"
 printf "  - version: %s\n" "${IMAGE_VERSION}"
 
 printf "\n--- Start Docker build\n\n"
-docker build . -t ghcr.io/ada-social-network/api:latest -t ghcr.io/ada-social-network/api:"${1}" --build-arg VERSION="${1}"
+docker build . -t "${IMAGE_LATEST}" -t "${IMAGE_VERSION}" --build-arg VERSION="${VERSION}"
 printf "\n\n--- End Docker build\n"
 
 printf "\nYou can now start using your image by running the following command\n\n"
@@ -17,3 +30,5 @@ printf "  docker run --rm %s" "${IMAGE_VERSION}"
 printf "\n\nYou can push your images by running the following command:\n\n"
 printf "  docker push %s\n" "${IMAGE_VERSION}"
 printf "  docker push %s\n" "${IMAGE_LATEST}"
+
+exit 0
