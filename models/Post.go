bf20cb8c7f0381b99f3b6046120aed3ae07adcfe
aliceKatkout <alice.katkout@gmail.com>
2022-01-21 15:04:22 +0100

    add forgetten post modif

--- models/Post.go
+++ models/Post.go
@@ -5,7 +5,6 @@ import uuid "github.com/satori/go.uuid"
 // Post define post resource
 type Post struct {
 	Base
-	Title   string `json:"title" binding:"required,min=4,max=100"`
 	Content string `json:"content" binding:"required,min=4,max=21474"`
 	// By default, gorm will try to use UserID as a foreign key to the model User
 	UserID  uuid.UUID `gorm:"type=uuid" json:"userId"`
