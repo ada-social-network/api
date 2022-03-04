6ff5c83a6f88524c0c60dc88a1776825a3a9797f
aliceKatkout <alice.katkout@gmail.com>
2022-03-04 14:47:26 +0100

    add profilePic to promo

--- models/Promo.go
+++ models/Promo.go
@@ -3,10 +3,11 @@ package models
 // Promo define a promo resource
 type Promo struct {
 	Base
-	PromoName string `json:"promo"`
-	StartDate string `json:"dateOfStart"`
-	EndDate   string `json:"dateOfEnd"`
-	Bio       string `json:"biography"`
+	Name       string `json:"name"`
+	StartDate  string `json:"dateOfStart"`
+	EndDate    string `json:"dateOfEnd"`
+	Bio        string `json:"biography"`
+	ProfilePic string `json:"profilePic"`
 	// By default, gorm will try to use UserID as a foreign key to the model User
 	Users []User `json:"users"`
 }
