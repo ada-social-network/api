86e57044a970d66169918d8bbc1537fe7e8e0622
aliceKatkout <alice.katkout@gmail.com>
2022-09-15 14:30:36 +0200

    lint

--- main.go
+++ main.go
@@ -28,23 +28,6 @@ const (
 	basePathAuth = "/auth"
 )
 
-// CORS used for adding cors support
-func CORS() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
-		c.Next()
-	}
-}
-
 func main() {
 	var wait time.Duration
 	var port int
